a496401122d1d77a0cfde1d08750bea94cb91667
DustyRoller <DustyRoller@github.com>
2022-08-20 21:09:12 +0100

    Added exception constuctors

--- SudokuSolver/SudokuSolverException.cs
+++ SudokuSolver/SudokuSolverException.cs
@@ -6,14 +6,32 @@ namespace SudokuSolver
     /// Exception class for any exception thrown within the project that
     /// relates to the solving of a Sudoku puzzle.
     /// </summary>
+    [Serializable]
     public class SudokuSolverException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SudokuSolverException"/> class.
+        /// </summary>
+        public SudokuSolverException()
+        {
+        }
+
         /// <summary>
         /// Initializes a new instance of the <see cref="SudokuSolverException"/> class.
         /// </summary>
         /// <param name="message">The message associated with the exception.</param>
         public SudokuSolverException(string message)
             : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SudokuSolverException"/> class.
+        /// </summary>
+        /// <param name="message">The message associated with the exception.</param>
+        /// <param name="innerException">The inner Excepton associated with the exception.</param>
+        public SudokuSolverException(string message, Exception innerException)
+            : base(message, innerException)
         {
         }
     }
